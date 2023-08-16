6afc9f9083c95dff62d1eb4f54a05cc5f2005011
Aaditya-brrt <denduluriaaditya@gmail.com>
2023-08-16 19:06:49 -0400

    new pages

--- components/player.js
+++ components/player.js
@@ -1,1 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
+const SpotifyWebApi = require("spotify-web-api-node");
 
+var spotifyApi = new SpotifyWebApi();
+
+
+const Player = () => {
+
+}
+
+export default Player;
---
+++ components/searchResults.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+
+
+const SearchResults = () => {
+  
+}
+
+export default SearchResults;
